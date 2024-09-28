eb864be5247bfbbe7dad133024640b7a6690c106
Artemii <Voronin.Artemii.2003@gmail.com>
2024-09-28 19:00:28 +0300

    Build, 2 responses, README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Install requirements before using
+```
+su root
+apt-get install gcc-c++ cmake boost-asio-devel libssl-devel nlohmann-json-devel
+```
--- main.cpp
+++ main.cpp
@@ -9,10 +9,17 @@ using namespace boost::beast;
 using namespace boost::asio;
 using namespace std;
 
-int main(int argc, char *argv[]){
-    const string host = "rdb.altlinux.org";
-    const string path = "/api/export/branch_binary_packages/p10";
-    const string port = "443";
+const string host = "rdb.altlinux.org";
+const string path = "/api/export/branch_binary_packages/";
+const string port = "443";
+
+template<typename T>
+int compare(T json_1, T json_2){
+    return 0;
+}
+
+int get_response(http::response<http::string_body> *res, string branch){
+    string full_path = path + branch;
     io_service svc;
     ssl::context ctx(ssl::context::sslv23_client);
     ssl::stream<ip::tcp::socket> ssocket = {svc, ctx};
@@ -20,13 +27,26 @@ int main(int argc, char *argv[]){
     auto it = resolver.resolve(host, port);
     connect(ssocket.lowest_layer(), it);
     ssocket.handshake(ssl::stream_base::handshake_type::client);
-    http::request<http::string_body> req = {http::verb::get, path, 11};
+    http::request<http::string_body> req = {http::verb::get, full_path, 11};
     req.set(http::field::host, host);
     http::write(ssocket, req);
-    http::response<http::string_body> res;
     flat_buffer buffer;
-    http::read(ssocket, buffer, res);
-    auto json = nlohmann::json::parse(res.body());
-    std::cout<<json<<std::endl;
+    http::read(ssocket, buffer, *res);
+    boost::system::error_code ec;
+    ssocket.shutdown();
+    ssocket.lowest_layer().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+    ssocket.lowest_layer().close();
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    http::response<http::string_body> res_1;
+    get_response(&res_1, argv[1]);
+    auto json_1 = nlohmann::json::parse(res_1.body());
+    std::cout<<res_1.base()<<std::endl;
+    http::response<http::string_body> res_2;
+    get_response(&res_2, argv[2]);
+    auto json_2 = nlohmann::json::parse(res_2.body());
+    std::cout<<res_2.base()<<std::endl;
     return 0;
 }
